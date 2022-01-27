962ae71412a76592457583135d531fd90cee3f65
unknown <mitrehealthcare@gmail.com>
2022-01-26 19:02:23 -0800

    select document-query

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+
+}
+
+let dog-name = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogImages.length; i++){
+    dog-name[i].text-align: center
+    
+}
+
